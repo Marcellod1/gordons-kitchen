7b700771a7fb4e862bcef4a2b5c4e47f40eec7c2
Marcello Di Benedetto <marcellodb17@gmail.com>
2021-09-26 20:49:07 -0600

    added howler cdn with a test mp3 on button click

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ var tally = 0;
 
 // Change img source to pushed down button on mouse down
 buttonImg.addEventListener("mousedown", event => {
+    tally++;
+    console.log(`The button has been clicked ${tally} times(s)`);
     buttonImg.src = "/resources/img/red-button-pressed.png";
+
+    var soundClip = new Howl({
+        src: ['/resources/sounds/waterphone.mp3']
+      });
+
+    soundClip.play();
 });
 
 
