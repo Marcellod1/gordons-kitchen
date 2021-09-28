9a7fd7117bc2a7c22cd45afecae9632b39aab377
Marcello Di Benedetto <marcellodb17@gmail.com>
2021-09-28 16:38:06 -0600

    animated indicator

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 $(document).ready(function(){
+
+    /* Animation Constants*/
+    const meterStartOffset = -40;
+    const meterEndOffset = 620;
+    const updateMsecs = 20;
+    
+    /* Animation Variables*/
+    // NOTE: Change these depending on the difficulty?
+    var offsetRate = 5;
+    var playing = true;
+
+
+    setInterval(function(play) {
+        if(!play){
+            return;
+        }
+
+        var currMeterOffset = parseInt($('#meter-indicator').css("left"));
+        var updateMeterOffset =  currMeterOffset + offsetRate;
+        
+        if(updateMeterOffset < meterEndOffset){
+            $('#meter-indicator').css("left", currMeterOffset + offsetRate);
+        }
+    }, updateMsecs, playing);
+
+
+    /* Button mousedown event */
     $("#button").mousedown(function(){
         $("#button").attr("src","resources/img/red-button-pressed.png");
         console.log("Button Pressed");
-
         var soundClip = new Howl({
             src: ['resources/sounds/waterphone.mp3']
         });
-
         soundClip.play();
     });
 
+    /* Button mouseup event */
     $("#button").mouseup(function(){
         $("#button").attr("src","resources/img/red-button.png");
         console.log("Button Up");
