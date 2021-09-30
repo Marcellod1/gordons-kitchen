13421d7f0f966b98fb0af7cfdcf32111da222029
Marcello Di Benedetto <marcellodb17@gmail.com>
2021-09-30 13:15:31 -0600

    refactoring - moved animation based variables and methods into an animation class

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-/* Global State */
-var playing = true;
+class MeterAnimation {
+    constructor(meterStartOffset, meterEndOffset, rate) {
+      this.startOffset = meterStartOffset;
+      this.endOffset = meterEndOffset;
+      this.offsetRate = rate;
+      this.isPlaying = true;
+    }
 
-/* Animation Constants */
-const meterStartOffset = -40;
-const meterEndOffset = 620;
-const updateMsecs = 20;
+    update(){
+        if(!this.isPlaying){
+            return;
+        }
+
+        var currMeterOffset = parseInt($('#meter-indicator').css("left"));
+        var updateMeterOffset =  currMeterOffset + this.offsetRate;
+
+        if(updateMeterOffset < this.endOffset){
+            $('#meter-indicator').css("left", currMeterOffset + this.offsetRate);
+        }
+    }
+
+    stop(){
+        this.isPlaying = false;
+    }
+
+    start(){
+        this.isPlaying = true;
+    }
+
+    reset(){
+        $('#meter-indicator').css("left", this.startOffset);
+    }
+}
+
 
 $(document).ready(function(){
+    /* Animation Constants */
+    const meterStartOffset = -40;
+    const meterEndOffset = 620;
+    const meterRate = 5;
+    const updateMillis = 20;
+
+    var anim = new MeterAnimation(meterStartOffset, meterEndOffset, meterRate);
+    
+    setInterval(function(){
+        anim.update()
+    }, updateMillis);
+
     /* Button mousedown event */
     $("#button").mousedown(function(){
         $("#button").attr("src","resources/img/red-button-pressed.png");
-        console.log("Button Pressed");
-
+        console.log("Button Down");
         var soundClip = new Howl({
             src: ['resources/sounds/waterphone.mp3']
         });
@@ -22,27 +60,11 @@ $(document).ready(function(){
     $("#button").mouseup(function(){
         $("#button").attr("src","resources/img/red-button.png");
         console.log("Button Up");
-        playing = false;
-    });
-
-
-    /* Animation */
-    var offsetRate = 5;
-    
-    setInterval(function(play) {
-        
-        if(!playing){
-            return;
-        }
-
-        var currMeterOffset = parseInt($('#meter-indicator').css("left"));
-        var updateMeterOffset =  currMeterOffset + offsetRate;
-        
-        if(updateMeterOffset < meterEndOffset){
-            $('#meter-indicator').css("left", currMeterOffset + offsetRate);
-        }
-    }, updateMsecs, playing);
-
-
-
+        anim.stop();
+    });    
 });
+
+
+
+
+
