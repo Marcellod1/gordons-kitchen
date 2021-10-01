ca293d15020d2909ac455f0c9d380c6965b2a3f9
Marcello Di Benedetto <marcellodb17@gmail.com>
2021-09-30 20:10:19 -0600

    acceptance regions implemented

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-class MeterAnimation {
-    constructor(meterStartOffset, meterEndOffset, rate) {
-      this.startOffset = meterStartOffset;
-      this.endOffset = meterEndOffset;
-      this.currOffset = meterStartOffset;
+class MeterGame{
 
-      this.offsetRate = rate;
-      this.isPlaying = true;
+    /* Constructor */
+    constructor(meterStartOffset, meterEndOffset, rate, acceptRegionSize) {
+        this.startOffset = meterStartOffset;
+        this.endOffset = meterEndOffset;
+        this.offsetRate = rate;
+        this.regionSize = acceptRegionSize;
+
+        this.acceptanceRegion = []
+        this.currOffset = meterStartOffset;
+        this.isPlaying = true;
+
+        this.randomizeAcceptanceRegion();
     }
+
 
     /* Increments the "meter-indicator" position each time this is called 
      * Returns the updated offset for the indicator
@@ -25,19 +32,51 @@ class MeterAnimation {
         return updateMeterOffset;
     }
 
+
     /* Stops the animation. Each subsequent call of this.update() will have no effect */
     stop(){
         this.isPlaying = false;
     }
+
 
     /* Starts the animation. Each subsequent call of this.update() will update the animation */
     start(){
         this.isPlaying = true;
     }
 
+
     /* Resets the "meter-indicator" position to the startig offset for the animation */
     reset(){
-        this.currMeterOffset = this.meterStartOffset;
+        this.currOffset = this.meterStartOffset;
+        this.randomizeAcceptanceRegion();
+    }
+
+    get meterLength(){
+        return Math.abs(this.endOffset - this.startOffset);
+    }
+
+
+    /* Randomizes the acceptance region offsets */
+    randomizeAcceptanceRegion(){
+        const min = this.startOffset + this.regionSize;
+        const max = this.endOffset - this.regionSize;
+        const regionStart = Math.floor(Math.random() * (max - min) + min);
+
+        this.acceptanceRegion = [regionStart, regionStart + this.regionSize];
+    }
+
+
+    /* checks if the currOffset for the indicator is within the acceptance region */
+    inAcceptanceRegion(){
+        if(this.currOffset <= this.acceptanceRegion[1] &&
+           this.currOffset >= this.acceptanceRegion[0])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
 
@@ -46,33 +85,40 @@ $(document).ready(function(){
     /* Animation Constants */
     const meterStartOffset = -40;
     const meterEndOffset = 620;
+    const meterRate = 5;
+    const acceptRegionSize = meterRate * 10;
+    const updateMillis = 10;
 
     /* Animation Variables */
-    var meterRate = 5;
-    var updateMillis = 20;
-    var anim = new MeterAnimation(meterStartOffset, meterEndOffset, meterRate);
-    
+    var game = new MeterGame(meterStartOffset, meterEndOffset, meterRate, acceptRegionSize);
+    console.log("Acceptance Region: " + game.acceptanceRegion);
+
     /* Update animation every "updateMillis" milliseconds*/
     setInterval(function(){
-        var newOffset = anim.update();
+        var newOffset = game.update();
         $('#meter-indicator').css("left", newOffset);
+        console.log( game.currOffset + " " + game.inAcceptanceRegion());
+
     }, updateMillis);
 
     /* Button mousedown event */
     $("#button").mousedown(function(){
         $("#button").attr("src","resources/img/red-button-pressed.png");
         console.log("Button Down");
-        var soundClip = new Howl({
-            src: ['resources/sounds/waterphone.mp3']
-        });
-        soundClip.play();
     });
 
     /* Button mouseup event */
     $("#button").mouseup(function(){
         $("#button").attr("src","resources/img/red-button.png");
         console.log("Button Up");
-        anim.stop();
+        game.stop();
+
+        // Check if the animation mouse indicator is in the region of acceptance
+        
+        var soundClip = new Howl({
+            src: ['resources/sounds/waterphone.mp3']
+        });
+        soundClip.play();
     });    
 });
 
